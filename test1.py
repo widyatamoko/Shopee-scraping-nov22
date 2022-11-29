76d8ad7d7c4550b78cfc71ec62fb8d8c37a2b0a7
widyatamoko <bimo.widyatamoko@gmail.com>
2022-11-29 17:05:10 +0700

    Update

--- test1.py
+++ test1.py
@@ -1,11 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC 
 import time
 import bs4
 import pandas as pd
@@ -93,7 +88,7 @@ for product in all_product:
     data_dict['title'] = title_text
     data_dict['price'] = price_text
     # data_dict['pricedc'] = pricedc_text
-    data_dict['terjual'] = terjual_text
+    data_dict['sales'] = terjual_text
     data_dict['link'] = product_link
     data_dict_list.append(data_dict)
 
